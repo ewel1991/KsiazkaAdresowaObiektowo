5248bca49b7735fd8e0027286b26fb0b4809f2ab
ewel1991 <ewelina.beben.programista@gmail.com>
2024-10-26 16:23:32 +0200

    Add registered user to file

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
+
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa;
     ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+
+
+
+
     return 0;
 }
